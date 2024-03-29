6b645c14c15078cccbd84a6fdbb26b9d74a5dfd5
Guillaume <guillaumebaillieux88@gmail.com>
2024-03-29 12:18:56 +0100

    maj

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,7 +1,58 @@
 <?php
 
-function getAllTheNews(){
-    $sql = "SELECT "
+function getAllNewsHomePage(PDO $connect): array|string
+{
+    $sql = "SELECT n.title, n.slug, SUBSTRING(n.content, 1, 280) AS content, n.date_published, 
+                   u.login, u.thename, 
+            GROUP_CONCAT(c.title SEPARATOR ' | ') AS categ_title, GROUP_CONCAT(c.slug SEPARATOR ' | ') AS categ_slug
+            
+	FROM news n
+	LEFT JOIN user u
+		ON n.user_iduser = u.iduser
+    LEFT JOIN news_has_category nhc
+        ON nhc.news_idnews = n.idnews
+    LEFT JOIN category c
+        ON nhc.category_idcategory = c.idcategory
+       
+
+-- Condition de récupération
+    WHERE n.is_published = 1
+-- on groupe par la clef de la table du FROM (news)
+    GROUP BY n.idnews
+    ORDER BY n.date_published DESC
+
+        ";
+
+    try{
+    
+    $query = $connect->query($sql);
+
+    // si pas de résultats () : string
+    if(!$query->rowCount()) return "Pas encore de message";
+    
+    $result = $query->fetchAll();
+
+    $query->closeCursor();
+
+    return $result; // : array
+
+    }catch(Exception $e){
+        return $e->getMessage(); // : string
+    }
+
 }
 
-Dans model/NewsModel.php, créez une fonction qui va charger toutes les News publiées avec le titre, la date de publication, les 250 premiers caractères de l'article (si possible ne pas couper dans les mots ), thename de l'auteur par ordre de publication décroissante. rajouter les catégories si possible (même liens que dans le menu).
+function cutTheText(string $text, int $nbCharacter=200, bool $cutWord= false): string
+{
+    // on coupe à la longueure indiquée
+    $output = substr($text,0,$nbCharacter);
+    // si on ne souhaite pas couper dans un mot 
+    if($cutWord===false){
+        // on prend la position du dernier espace dans le texte(chaine)  
+        $lastSpace = stripos(" ", $output);
+        // on recoupe la chaine pour l'envoi
+        $output = substr($output,0,$lastSpace);
+
+    }
+    return $output;
+}
