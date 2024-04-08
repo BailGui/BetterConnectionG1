cd56bc1e6b946b5c94beddf9f5fec26f9ea0db52
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-08 13:46:14 +0200

    majNews

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,26 +1,77 @@
 <?php
 
-//Dans model/NewsModel.php, créez une fonction qui va charger toutes les News publiées avec le titre, la date de publication, les 250 premiers caractères 
-//de l'article (si possible ne pas couper dans les mots ), thename de l'auteur par ordre de publication décroissante. rajouter les catégories si possible (même liens que dans le menu).
+/**
+ * getAllNewsHomePage
+ *
+ * @param  PDO $connect
+ * @return array|string
+ */
+function getAllNewsHomePage(PDO $connect): array|string
+{
+    $sql = "SELECT n.`title`, n.`slug`, SUBSTRING(n.`content`, 1, 260) AS content, n.`date_published`, 
+                   u.`login`, u.`thename`,
+                   -- champs de category concaténés
+                   GROUP_CONCAT(c.`title` SEPARATOR '|||') AS categ_title,
+                   GROUP_CONCAT(c.`slug` SEPARATOR '|||') AS categ_slug 
+	FROM `news` n
+	LEFT JOIN `user` u
+		ON n.`user_iduser` = u.`iduser`
+-- on va sélectionner les champs title (as categ_title) et slug (as categ_slug) de la table category qu'il y ait des catégories ou pas
+    LEFT JOIN `news_has_category` h
+        ON h.`news_idnews` = n.`idnews`
+    LEFT JOIN `category` c
+        ON h.`category_idcategory` = c.`idcategory`
 
-function getAllTheNews(PDO $db){
 
-    $sql = "SELECT slug, n.title, n.date_published, SUBSTRING(content, 1, 250) AS cont, u.thename
-    FROM news n JOIN user u ON u.iduser = n.user_iduser
-    ORDER BY n.date_published DESC
-    ";
+-- Condition de récupération
+    WHERE n.`is_published` = 1
+-- on groupe par la clef de la table du FROM (news)
+    GROUP BY n.`idnews`    
+    ORDER BY n.`date_published` DESC
 
-try{
+        ;";
 
-    $query = $db->query($sql);
+    try{
+    
+    $query = $connect->query($sql);
+
+    // si pas de résultats () : string
+    if(!$query->rowCount()) return "Pas encore de message";
+    
     $result = $query->fetchAll();
+
     $query->closeCursor();
-    return $result;
 
-}catch(Exception $e){
+    return $result; // : array
 
-        return $e->getMessage();
+    }catch(Exception $e){
+        return $e->getMessage(); // : string
+    }
+
 }
-}
 
+/**
+ * cutTheText
+ *
+ * @param  string $text
+ * @param  int $nbCharacter
+ * @param  bool $cutWord
+ * @return string
+ */
+function cutTheText(string $text, int $nbCharacter=200, bool $cutWord = false): string
+{
+    // on coupe à la longueure indiquée
+    $output = substr($text,0,$nbCharacter);
+
+    // si on ne souhaîte pas couper dans un mot
+    if($cutWord===false){
+        // on prend la position du dernier espace dans la chaîne
+        $lastSpace = strrpos($output, " ");
+
+        // ou recoupe la chaîne pour l'envoie
+        $output = substr($output,0,$lastSpace);
+    }
+
+    return $output;
+}
 
